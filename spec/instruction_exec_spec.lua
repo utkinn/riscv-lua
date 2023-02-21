67a36e238a07cc886c740c5d28e0a533c0eadb9d
javabird25 <shockck84@gmail.com>
2023-02-21 13:03:36 +0500

    Extract some "add" test code to before_each

--- spec/instruction_exec_spec.lua
+++ spec/instruction_exec_spec.lua
@@ -6,75 +6,72 @@ local getOpcodeAndFunc3ForMnemonic = instructions.getOpcodeAndFunc3ForMnemonic
 
 describe("Instruction", function()
     describe(":exec(cpu, memory)", function()
+        local cpu
+
+        before_each(function()
+            cpu = CPU.new()
+        end)
+
         describe("Arithmetic with registers", function()
             describe("add", function()
-                it("regular", function()
-                    local cpu = CPU.new()
-                    cpu.registers[1] = 1
-                    cpu.registers[2] = 2
+                local instructionData
+
+                before_each(function()
                     local opcode, funct3 = getOpcodeAndFunc3ForMnemonic("add")
-                    local inst = Instruction.new {
+                    instructionData = {
                         opcode = opcode,
                         funct3 = funct3,
-                        rd = 1,
-                        rs1 = 1,
-                        rs2 = 2,
-                        funct7 = 0
+                        funct7 = 0,
                     }
-                    inst:exec(cpu, nil)
+                end)
+
+                it("regular", function()
+                    cpu.registers[1] = 1
+                    cpu.registers[2] = 2
+                    instructionData.rd = 1
+                    instructionData.rs1 = 1
+                    instructionData.rs2 = 2
+                    local inst = Instruction.new(instructionData)
+
+                    inst:exec(cpu)
 
                     assert.are.equal(3, cpu.registers[1])
                 end)
 
                 it("with x0", function()
-                    local cpu = CPU.new()
                     cpu.registers[1] = 0
                     cpu.registers[2] = 1
-                    local opcode, funct3 = getOpcodeAndFunc3ForMnemonic("add")
-                    local inst = Instruction.new {
-                        opcode = opcode,
-                        funct3 = funct3,
-                        rd = 1,
-                        rs1 = 2,
-                        rs2 = 0,
-                        funct7 = 0
-                    }
-                    inst:exec(cpu, nil)
+                    instructionData.rd = 1
+                    instructionData.rs1 = 2
+                    instructionData.rs2 = 0
+                    local inst = Instruction.new(instructionData)
+
+                    inst:exec(cpu)
 
                     assert.are.equal(1, cpu.registers[1])
                 end)
 
                 it("no-op if rd is x0", function()
-                    local cpu = CPU.new()
                     cpu.registers[1] = 1
-                    local opcode, funct3 = getOpcodeAndFunc3ForMnemonic("add")
-                    local inst = Instruction.new {
-                        opcode = opcode,
-                        funct3 = funct3,
-                        rd = 0,
-                        rs1 = 1,
-                        rs2 = 1,
-                        funct7 = 0
-                    }
-                    inst:exec(cpu, nil)
+                    instructionData.rd = 0
+                    instructionData.rs1 = 1
+                    instructionData.rs2 = 1
+                    local inst = Instruction.new(instructionData)
+
+                    inst:exec(cpu)
 
                     assert.are.equal(0, cpu.registers[0])
                 end)
 
                 it("with overflow", function()
-                    local cpu = CPU.new()
                     cpu.registers[1] = 0xFFFFFFFF
                     cpu.registers[2] = 1
-                    local opcode, funct3 = getOpcodeAndFunc3ForMnemonic("add")
-                    local inst = Instruction.new {
-                        opcode = opcode,
-                        funct3 = funct3,
-                        rd = 1,
-                        rs1 = 1,
-                        rs2 = 2,
-                        funct7 = 0
-                    }
-                    inst:exec(cpu, nil)
+                    instructionData.rd = 1
+                    instructionData.rs1 = 1
+                    instructionData.rs2 = 2
+                    local inst = Instruction.new(instructionData)
+
+                    inst:exec(cpu)
 
                     assert.are.equal(0, cpu.registers[1])
                 end)
