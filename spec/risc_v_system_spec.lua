97f0e82d6fcaf8d49c4fd21828863f192be49334
javabird25 <shockck84@gmail.com>
2023-03-11 13:13:38 +0500

    System:{load,runTicks}

--- spec/risc_v_system_spec.lua
+++ spec/risc_v_system_spec.lua
@@ -8,4 +8,28 @@ describe("System", function()
 
         assert.are.equal(100, sys.memory:size())
     end)
+
+    it("can load data into memory", function()
+        local sys = System.new {
+            memorySize = 100
+        }
+
+        sys:load(0, "hello")
+        assert.are.equal("hello", sys.memory:read(0, 5))
+    end)
+
+    it("can run a simple program", function()
+        local sys = System.new {
+            memorySize = 100
+        }
+
+        -- addi x1, x0, 10
+        -- addi x1, x1, -5
+        sys:load(0, string.char(0x93, 0x00, 0xa0, 0x00, 0x93, 0x80, 0xb0, 0xff))
+
+        sys:runTicks(1)
+        assert.are.equal(10, sys.cpu.registers[1])
+        sys:runTicks(1)
+        assert.are.equal(5, sys.cpu.registers[1])
+    end)
 end)
--- src/risc_v_system.lua
+++ src/risc_v_system.lua
@@ -14,4 +14,14 @@ function mod.System.new(spec)
     return setmetatable(self, { __index = mod.System })
 end
 
+function mod.System:load(address, data)
+    self.memory:write(address, data)
+end
+
+function mod.System:runTicks(ticks)
+    for i = 1, ticks do
+        self.cpu:tick(self.memory)
+    end
+end
+
 return mod
